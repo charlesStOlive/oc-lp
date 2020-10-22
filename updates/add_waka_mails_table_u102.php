8a9918129efe4294e8b9eed92a008f8b917f86f0
Charles <charles.stolive@gmail.com>
2020-10-22 10:06:37 +0200

    correction bug artisan:up

--- updates/add_waka_mails_table_u102.php
+++ updates/add_waka_mails_table_u102.php
@@ -4,26 +4,31 @@ use October\Rain\Database\Schema\Blueprint;
 use October\Rain\Database\Updates\Migration;
 use Schema;
 
-class CreateWakaMailsTableU102 extends Migration
+class AddWakaMailsTableU102 extends Migration
 {
     public function up()
     {
-        Schema::table('waka_mailer_wakamails', function (Blueprint $table) {
-            $table->boolean('use_lp')->nullable();
-            $table->string('lp')->nullable();
-            $table->boolean('use_key')->nullable();
-            $table->string('key_duration')->nullable();
+        if (Schema::hasTable('waka_mailer_wakamails')) {
+            Schema::table('waka_mailer_wakamails', function (Blueprint $table) {
+                $table->boolean('use_lp')->nullable();
+                $table->string('lp')->nullable();
+                $table->boolean('use_key')->nullable();
+                $table->string('key_duration')->nullable();
 
-        });
+            });
+        }
+
     }
 
     public function down()
     {
-        Schema::table('waka_mailer_wakamails', function (Blueprint $table) {
-            $table->dropColumn('use_lp');
-            $table->dropColumn('lp');
-            $table->dropColumn('use_key');
-            $table->dropColumn('key_duration');
-        });
+        if (Schema::hasTable('waka_mailer_wakamails')) {
+            Schema::table('waka_mailer_wakamails', function (Blueprint $table) {
+                $table->dropColumn('use_lp');
+                $table->dropColumn('lp');
+                $table->dropColumn('use_key');
+                $table->dropColumn('key_duration');
+            });
+        }
     }
 }
--- updates/add_waka_mailtos_table_U103.php
+++ updates/add_waka_mailtos_table_U103.php
@@ -8,22 +8,26 @@ class AddWakaMailtosTableU103 extends Migration
 {
     public function up()
     {
-        Schema::table('waka_mailtoer_waka_mailtos', function (Blueprint $table) {
-            $table->boolean('use_lp')->nullable();
-            $table->string('lp')->nullable();
-            $table->boolean('use_key')->nullable();
-            $table->string('key_duration')->nullable();
+        if (Schema::hasTable('waka_mailtoer_waka_mailtos')) {
+            Schema::table('waka_mailtoer_waka_mailtos', function (Blueprint $table) {
+                $table->boolean('use_lp')->nullable();
+                $table->string('lp')->nullable();
+                $table->boolean('use_key')->nullable();
+                $table->string('key_duration')->nullable();
 
-        });
+            });
+        }
     }
 
     public function down()
     {
-        Schema::table('waka_mailtoer_waka_mailtos', function (Blueprint $table) {
-            $table->dropColumn('use_lp');
-            $table->dropColumn('lp');
-            $table->dropColumn('use_key');
-            $table->dropColumn('key_duration');
-        });
+        if (Schema::hasTable('waka_mailtoer_waka_mailtos')) {
+            Schema::table('waka_mailtoer_waka_mailtos', function (Blueprint $table) {
+                $table->dropColumn('use_lp');
+                $table->dropColumn('lp');
+                $table->dropColumn('use_key');
+                $table->dropColumn('key_duration');
+            });
+        }
     }
 }
